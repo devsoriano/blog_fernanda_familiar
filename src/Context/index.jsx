8d9831740f91da282445d6a93c0777a60cf3c6af
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2024-07-01 08:02:13 -0600

    [Add]: Salvar consulta en el navegador

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,19 +1,41 @@
+/* eslint-disable react/prop-types */
 import { createContext, useEffect, useState } from "react";
 
 export const BlogContext = createContext();
 
-// eslint-disable-next-line react/prop-types
 export const BlogProvider = ({ children }) => {
-  // Obtener publicaciones
   const [items, setItems] = useState([]);
 
+  const saveToLocalStorage = (data) => {
+    localStorage.setItem("blogItems", JSON.stringify(data));
+  };
+
+  const getFromLocalStorage = () => {
+    const storedItems = localStorage.getItem("blogItems");
+    return storedItems ? JSON.parse(storedItems) : [];
+  };
+
   useEffect(() => {
-    fetch("https://fernandafamiliar.soy/wp-json/wp/v2/posts?per_page=10")
-      .then((response) => response.json())
-      .then((data) => setItems(data));
+    const storedItems = getFromLocalStorage();
+    if (storedItems.length > 0) {
+      setItems(storedItems);
+    } else {
+      fetch("https://fernandafamiliar.soy/wp-json/wp/v2/posts?per_page=10")
+        .then((response) => response.json())
+        .then((data) => {
+          setItems(data);
+          saveToLocalStorage(data);
+        });
+    }
   }, []);
 
+  useEffect(() => {
+    if (items.length > 0) {
+      saveToLocalStorage(items);
+    }
+  }, [items]);
+
   return (
-    <BlogContext.Provider value={{ items }}> {children}</BlogContext.Provider>
+    <BlogContext.Provider value={{ items }}>{children}</BlogContext.Provider>
   );
 };
